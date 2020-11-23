16e944811d6f9b5cb2c93fb121605ca25dc892f8
dhencios15 <ddelacruz@umindanao.edu.ph>
2020-11-23 10:08:44 +0800

    add prod url

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import cookies from 'vue-cookies'
 
-// const prodURL = 'https://calm-ocean-73108.herokuapp.com/api/v1/'
+const prodURL = 'https://calm-ocean-73108.herokuapp.com/api/v1/'
 
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:3000/api/v1/`,
+    baseURL: prodURL,
     headers: {
       Authorization: `Bearer ${cookies.get('jwt')}`
     }
